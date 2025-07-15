f26944f7881be4a8e2fc44ff7a5ee97f012e21c2
Frank Zhang <rgb360@126.com>
2025-07-15 17:07:16 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -17,6 +17,7 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
+
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
