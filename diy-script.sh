91950c99cda2b0c54d9f5640eda725f8bbba1e94
Frank Zhang <rgb360@126.com>
2025-07-15 16:41:14 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -11,6 +11,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -73,7 +75,7 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-# git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/geodata 
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
